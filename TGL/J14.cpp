ccefdf9e9bf183dc22d41a19b14b78fb789ecc1e
JustinR840 <ramosjus@sonoma.edu>
2018-04-05 21:30:25 -0700

    Added error recovery.

--- TGL/J14.cpp
+++ TGL/J14.cpp
@@ -1,5 +1,5 @@
 /********************************************************************************
-* File: J11.cpp                                                                 *
+* File: J14.cpp                                                                 *
 * Author: Colin Franceschini, Justin Ramos                                      *
 * Date: 4/4/18                                                                  *
 * Description: This is the present verb function that will be used to parse     *
@@ -7,10 +7,22 @@
 * Comments:                                                                     *
 ********************************************************************************/
 
+#include <set>
 #include "Syn.h"
 
 int Syn::present_verb() {  // Function 14
+	static set<string> firsts = {"is", "masquerades-as"};
+	static set<string> follows = {"a"};
+	
 	int errors = 0;
+
+	while(!firsts.count(ct) && !follows.count(ct))
+	{
+		lex->ReportError("unexpected '" + ct + "' found at beginning of <present_verb>.");
+		errors++;
+		ct = lex->NextTerminal();
+	}
+
 	// We expect a terminal state of present verb to be here.
 	if (ct == "is" || ct == "masquerades-as") {
 		ct = lex->NextTerminal();
